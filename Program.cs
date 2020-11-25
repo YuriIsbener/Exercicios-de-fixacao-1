6c22bf80dbba1631414410e19cd140a589db1d41
Yuri Isbener <yurisuhett@gmail.com>
2020-11-24 21:49:53 -0300

    trab feito

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exercicios_de_fixação
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int idade;
+
+            Console.WriteLine("Em que ano estamos?");
+            int anoAtual = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Em que ano você nasceu?");
+            int anoNascimento = int.Parse(Console.ReadLine());
+
+            idade = anoAtual - anoNascimento;
+
+            if (idade < 16)
+            {
+                Console.WriteLine("Você não atende os requerimentos para votar");
+            }
+            else
+            {
+                Console.WriteLine("Você atende os requerimentos para votar");
+            }
+
+
+        }
+    }
+}
